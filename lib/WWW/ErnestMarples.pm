86c8e1109c29917eb91a3a85fef262d2d4a1b0ea
Andy Armstrong <andy@hexten.net>
2009-07-09 14:07:09 +0100

    Initial check-in.

---
+++ lib/WWW/ErnestMarples.pm
@@ -0,0 +1,62 @@
+package WWW::ErnestMarples;
+
+use warnings;
+use strict;
+
+use Carp;
+
+=head1 NAME
+
+WWW::ErnestMarples - [One line description of module's purpose here]
+
+=head1 VERSION
+
+This document describes WWW::ErnestMarples version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+  use WWW::ErnestMarples;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< new >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+WWW::ErnestMarples requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+Please report any bugs or feature requests to
+C<bug-www-ernestmarples@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2009, Andy Armstrong  C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
